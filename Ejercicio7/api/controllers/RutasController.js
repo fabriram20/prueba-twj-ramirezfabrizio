e5dbb5a595014b31af5624a37c7c77c931e13224
fabriram20 <fabriram20@gmail.com>
2017-03-02 22:02:34 -0500

    views listar usuarios

--- Ejercicio7/api/controllers/RutasController.js
+++ Ejercicio7/api/controllers/RutasController.js
@@ -7,22 +7,58 @@
 
 module.exports = {
 
-    home: function (req, res) {
-        return res.view('home');
-    },
-    error: function (req, res) {
-        return res.view('error', {
+  home: function (req, res) {
+    return res.view('home');
+  },
+  error: function (req, res) {
+    return res.view('error', {
+      error: {
+        desripcion: "Usted esta por error en esta Ruta dirijase a Inicio",
+        rawError: "Ruta equivocada",
+        url: "/home"
+      }
+    });
+  },
+  crearUsuario: function (req, res) {
+    return res.view('Usuario/crearUsuario');
+  },
+  crearBorrachera: function (req, res) {
+    return res.view('Borrachera/crearBorrachera');
+  },
+
+  listarUsuarios: function (req, res) {
+    Usuario.find()
+      .exec(function (errorIndefinido, usuariosEncontrados) {
+        if (errorIndefinido) {
+          res.view('Error', {
             error: {
-                desripcion: "Usted esta por error en esta Ruta dirijase a Inicio",
-                rawError: "Ruta equivocada",
-                url: "/home"
+              desripcion: "Hubo un problema listando los Usuarios",
+              rawError: errorIndefinido,
+              url: "/ListarUsuarios"
             }
+          });
+        }
+        res.view('Usuario/ListarUsuario', {
+          usuarios: usuariosEncontrados
         });
-    },
-    crearUsuario: function (req, res) {
-        return res.view('Usuario/crearUsuario');
-    },
-    crearBorrachera: function (req, res) {
-        return res.view('Borrachera/crearBorrachera');
-    }
+      })
+  },
+
+  listarBorracheras: function (req, res) {
+    Borrachera.find()
+      .exec(function (errorIndefinido, borracherasEncontradas) {
+        if (errorIndefinido) {
+          res.view('Error', {
+            error: {
+              desripcion: "Hubo un problema listando las borracheras",
+              rawError: errorIndefinido,
+              url: "/ListarBorracheras"
+            }
+          });
+        }
+        res.view('Borrachera/ListarBorrachera', {
+          borracheras: borracherasEncontradas
+        });
+      })
+  }
 };
